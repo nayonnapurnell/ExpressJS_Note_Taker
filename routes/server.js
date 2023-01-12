da4cef4d84ce485e502360cf995fef5a6eb726e9
“Nayonna <Nayonna Purnell>
2023-01-12 07:35:12 -0500

    Added a working index.html route

--- routes/server.js
+++ routes/server.js
@@ -1,10 +1,18 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
-app.listen(3001, listening);
+app.listen(3002, listening);
 
 function listening(){
-    console.log("Server is starting...");
+    console.log("Note Taker is starting...");
 }
 
-app.use(express.static('website'));
+app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.send('Navigate to /send or /routes')
+  });
+
+  app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
